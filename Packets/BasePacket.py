a302be1d74df578a4e101c250dad48815837ce0e
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2019-03-24 13:22:34 +0100

    Add Methods and properties in BasePacket Modify Enum Classes
    Signed-off-by: Gabriel Desfrene <desfrene.gabriel@gmail.com>

--- Packets/BasePacket.py
+++ Packets/BasePacket.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from Packets import Enums
 
 
 class Private:
@@ -36,16 +37,45 @@ class Private:
 
 
 class BasePacket:
-    def _init__(self):
-        self._type = 0x0
-        self._sub_type = "00"
-        self._is_extended = False
-        self._data = None
-
+    def __init__(self):
+        self._type = Enums.PacketType.Unknown
+        self._sub_type = Enums.PacketSubType.Default
+        self._data = bytearray()
         self._buffer = bytearray()
 
+    @property
+    def extended(self) -> bool:
+        return len(self._data) != 0
+
+    @property
+    def packet_type(self) -> Enums.PacketType:
+        return self._type
+
+    @property
+    def packet_subtype(self) -> Enums.PacketSubType:
+        return self._sub_type
+
+    @property
+    def checksum(self) -> int:
+        self.compute_checksum()
+        return Private.to_integer(self._buffer[-2:])
+
+    @property
+    def data(self) -> bytes:
+        return bytes(self._data)
+
     def compute_checksum(self):
-        self._update_buffer()
+        self._buffer.clear()
+
+        self._buffer.append(self._type.value)
+        self._buffer += self._sub_type.value
+
+        if self.extended:
+            self._buffer += b"1"
+            self._buffer += Private.to_hexadecimal(len(self._data), 4)
+            self._buffer += self._data
+        else:
+            self._buffer += b"0"
 
         checksum = 0
         for i in self._buffer[1:]:
@@ -54,25 +84,35 @@ class BasePacket:
         checksum = ~checksum + 1
         checksum %= 256
 
-        self._data += Private.to_hexadecimal(checksum)
-
-    def _update_buffer(self):
-        self._buffer.clear()
-
-        self._buffer.append(self._type)
-        # TODO : Check This
-        self._buffer += bytearray(self._sub_type[0:2], "ascii")
-
-        if self._is_extended:
-            self._buffer += b"1"
-            self._buffer += Private.to_hexadecimal(len(self._data), 4)
-            self._buffer += self._data
-        else:
-            self._buffer += b"0"
+        self._buffer += Private.to_hexadecimal(checksum)
 
     def is_ready_to_send(self) -> bool:
-        if self._is_extended:
-            # TODO : Check This
-            return len(self._data) == 6 + Private.to_integer(self._data[4:8])
+        if self.extended:
+            return len(self._buffer) == 10 + Private.to_integer(self._buffer[4:8])
         else:
-            return len(self._data) == 6
+            return len(self._buffer) == 6
+
+    def __repr__(self):
+        self.compute_checksum()
+        if self.extended:
+            return "Type : {:#02x}, Subtype : {}, Extended : True, Data : {}, Checksum : {}".format(
+                self._type.value, self._sub_type.value.decode("ascii"), self._data, self._buffer[-2:].decode("ascii"))
+        else:
+            return "Type : {:#02x}, Subtype : {}, Extended : False, Checksum : {}".format(
+                self._type.value, self._sub_type.value.decode("ascii"), self._buffer[-2:].decode("ascii"))
+
+    def __bytes__(self):
+        self.compute_checksum()
+        return bytes(self._buffer)
+
+    def _set_type(self, packet_type: Enums.PacketType):
+        if not isinstance(packet_type, Enums.PacketType):
+            raise TypeError
+
+        self._type = packet_type.value
+
+    def _set_subtype(self, packet_subtype: Enums.PacketSubType):
+        if not isinstance(packet_subtype, Enums.PacketSubType):
+            raise TypeError
+
+        self._sub_type = packet_subtype
--- Packets/Enums.py
+++ Packets/Enums.py
@@ -3,155 +3,97 @@
 from enum import Enum
 
 
-class PacketType(Enum):
-    Command = 0x01
-    Data = 0x02
-    Roleswap = 0x03
-    Check = 0x05
-    Ack = 0x06
-    Error = 0x15
-    Terminate = 0x18
+class PacketType(Enum):  # [0 - 24]
+    Unknown = 0x00  # Value : 0
+
+    Command = 0x01  # Value : 1
+    Data = 0x02  # Value : 2
+    Roleswap = 0x03  # Value : 3
+    Check = 0x05  # Value : 5
+    Ack = 0x06  # Value : 6
+    Error = 0x15  # Value : 21
+    Terminate = 0x18  # Value : 24
 
 
-class CommandPacketType:
-    class System(Enum):
-        Reset = "00"
-        GetInfo = "01"
-        SetLinkSettings = "03"
+class PacketSubType(Enum):
+    # Default (Roleswap, Ack, Error, Terminate)
+    Default = b"00"  # Value : 0
 
-    class MCSStorage(Enum):
-        CreateDirectory = "20"
-        DeleteDirectory = "21"
-        RenameDirectory = "22"
-        ChangeWorkingDirectory = "23"
+    # Check Types              # [0 - 1]
+    InitConnection = b"00"  # Value : 0
+    CheckConnection = b"01"  # Value : 1
 
-        FileTransferRequest = "24"
-        FileTransferAllRequest = "29"
-        FileTransfer = "25"
+    # Ack Types                # [0 - 2]
+    YesOverwriteReply = b"01"  # Value : 1
+    ExtendedAck = b"02"  # Value : 2
 
-        DeleteFile = "26"
-        RenameFile = "27"
-        CopyFile = "28"
+    # Error Types              # [0 - 5]
+    ResendRequest = b"01"  # Value : 1
+    OverwriteError = b"02"  # Value : 2
+    NoOverwriteReply = b"03"  # Value : 3
+    OverwriteImpossible = b"04"  # Value : 4
+    MemoryFull = b"05"  # Value : 5
 
-        CapacityTransmitRequest = "2"
-        CapacityTransmit = "2C"
+    # Terminates Types         # [0 - 3]
+    UserRequest = b"01"  # Value : 1
+    Timeout = b"02"  # Value : 2
+    Overwrite = b"03"  # Value : 3
 
-        FileInfoTransferAllRequest = "2D"
-        FileInfoTransfer = "2E"
+    # Commands & Data Types
 
-        RAMImageTransferRequest = "2F"
-        RAMImageTransfer = "30"
+    # System                   # [0 - 2]
+    Reset = b"00"  # Value : 0
+    GetInfo = b"01"  # Value : 1
+    SetLinkSettings = b"02"  # Value : 2
 
-        SetupEntryTransferRequest = "31"
-        SetupEntryTransfer = "32"
-        SetupEntryTransferAllRequest = "33"
+    # MCS Storage              # [32 - 51]
+    MCSCreateDirectory = b"20"  # Value : 32
+    MCSDeleteDirectory = b"21"  # Value : 33
+    MCSRenameDirectory = b"22"  # Value : 34
+    MCSChangeWorkingDirectory = b"23"  # Value : 35
 
-    class FlashStorage(Enum):
-        CreateDirectory = "40"
-        DeleteDirectory = "41"
-        RenameDirectory = "42"
-        ChangeWorkingDirectory = "43"
+    MCSFileTransferRequest = b"24"  # Value : 36
+    MCSFileTransferAllRequest = b"29"  # Value : 41
+    MCSFileTransfer = b"25"  # Value : 37
 
-        FileTransferRequest = "44"
-        FileTransfer = "45"
-        FileTransferAllRequest = "49"
+    MCSDeleteFile = b"26"  # Value : 38
+    MCSRenameFile = b"27"  # Value : 39
+    MCSCopyFile = b"28"  # Value : 40
 
-        DeleteFile = "46"
-        RenameFile = "47"
-        CopyFile = "48"
+    MCSCapacityTransmitRequest = b"2B"  # Value : 43
+    MCSCapacityTransmit = b"2C"  # Value : 44
 
-        CapacityTransmitRequest = "4"
-        CapacityTransmit = "4C"
+    MCSFileInfoTransferAllRequest = b"2D"  # Value : 45
+    MCSFileInfoTransfer = b"2E"  # Value : 46
 
-        FileInfoTransferAllRequest = "4D"
-        FileInfoTransfer = "4E"
+    MCSImageTransferRequest = b"2F"  # Value : 47
+    MCSImageTransfer = b"30"  # Value : 48
 
-        FlashImageTransferRequest = "4F"
-        FlashImageTransfer = "50"
+    MCSSetupEntryTransferRequest = b"31"  # Value : 49
+    MCSSetupEntryTransfer = b"32"  # Value : 50
+    MCSSetupEntryTransferAllRequest = b"33"  # Value : 51
 
-        OptimizeFileSystem = "51"
+    # FlashStorage Types       # [64 - 81]
+    FlashCreateDirectory = b"40"  # Value : 64
+    FlashDeleteDirectory = b"41"  # Value : 65
+    FlashRenameDirectory = b"42"  # Value : 66
+    FlashChangeWorkingDirectory = b"43"  # Value : 67
 
+    FlashFileTransferRequest = b"44"  # Value : 68
+    FlashFileTransfer = b"45"  # Value : 69
+    FlashFileTransferAllRequest = b"49"  # Value : 73
 
-class DataPacketType:
-    class MCSStorage(Enum):
-        CreateDirectory = CommandPacketType.MCSStorage.CreateDirectory
-        DeleteDirectory = CommandPacketType.MCSStorage.DeleteDirectory
-        RenameDirectory = CommandPacketType.MCSStorage.RenameDirectory
-        ChangeWorkingDirectory = CommandPacketType.MCSStorage.ChangeWorkingDirectory
+    FlashDeleteFile = b"46"  # Value : 70
+    FlashRenameFile = b"47"  # Value : 71
+    FlashCopyFile = b"48"  # Value : 72
 
-        FileTransferRequest = CommandPacketType.MCSStorage.FileTransferRequest
-        FileTransferAllRequest = CommandPacketType.MCSStorage.FileTransferAllRequest
-        FileTransfer = CommandPacketType.MCSStorage.FileTransfer
+    FlashCapacityTransmitRequest = b"4B"  # Value : 75
+    FlashCapacityTransmit = b"4C"  # Value : 76
 
-        DeleteFile = CommandPacketType.MCSStorage.DeleteFile
-        RenameFile = CommandPacketType.MCSStorage.RenameFile
-        CopyFile = CommandPacketType.MCSStorage.CopyFile
+    FlashFileInfoTransferAllRequest = b"4D"  # Value : 77
+    FlashFileInfoTransfer = b"4E"  # Value : 78
 
-        CapacityTransmitRequest = CommandPacketType.MCSStorage.CapacityTransmitRequest
-        CapacityTransmit = CommandPacketType.MCSStorage.CapacityTransmit
+    FlashImageTransferRequest = b"4F"  # Value : 79
+    FlashImageTransfer = b"50"  # Value : 80
 
-        FileInfoTransferAllRequest = CommandPacketType.MCSStorage.FileInfoTransferAllRequest
-        FileInfoTransfer = CommandPacketType.MCSStorage.FileInfoTransfer
-
-        RAMImageTransferRequest = CommandPacketType.MCSStorage.RAMImageTransferRequest
-        RAMImageTransfer = CommandPacketType.MCSStorage.RAMImageTransfer
-
-        SetupEntryTransferRequest = CommandPacketType.MCSStorage.SetupEntryTransferRequest
-        SetupEntryTransfer = CommandPacketType.MCSStorage.SetupEntryTransfer
-        SetupEntryTransferAllRequest = CommandPacketType.MCSStorage.SetupEntryTransferAllRequest
-
-    class FlashStorage(Enum):
-        CreateDirectory = CommandPacketType.FlashStorage.CreateDirectory
-        DeleteDirectory = CommandPacketType.FlashStorage.DeleteDirectory
-        RenameDirectory = CommandPacketType.FlashStorage.RenameDirectory
-        ChangeWorkingDirectory = CommandPacketType.FlashStorage.ChangeWorkingDirectory
-
-        FileTransferRequest = CommandPacketType.FlashStorage.FileTransferRequest
-        FileTransferAllRequest = CommandPacketType.FlashStorage.FileTransferAllRequest
-        FileTransfer = CommandPacketType.FlashStorage.FileTransfer
-
-        DeleteFile = CommandPacketType.FlashStorage.DeleteFile
-        RenameFile = CommandPacketType.FlashStorage.RenameFile
-        CopyFile = CommandPacketType.FlashStorage.CopyFile
-
-        CapacityTransmitRequest = CommandPacketType.FlashStorage.CapacityTransmitRequest
-        CapacityTransmit = CommandPacketType.FlashStorage.CapacityTransmit
-
-        FileInfoTransferAllRequest = CommandPacketType.FlashStorage.FileInfoTransferAllRequest
-        FileInfoTransfer = CommandPacketType.FlashStorage.FileInfoTransfer
-
-        FlashImageTransferRequest = CommandPacketType.FlashStorage.FlashImageTransferRequest
-        FlashImageTransfer = CommandPacketType.FlashStorage.FlashImageTransfer
-
-        OptimizeFileSystem = CommandPacketType.FlashStorage.OptimizeFileSystem
-
-
-class RoleswapPacketType(Enum):
-    Default = "00"
-
-
-class CheckPacketType(Enum):
-    InitConnection = "00"
-    CheckConnection = "01"
-
-
-class AckPacketType(Enum):
-    Default = "00"
-    YesOverwriteReply = "01"
-    ExtendedAck = "03"
-
-
-class ErrorPacketType(Enum):
-    Default = "00"
-    ResendRequest = "01"
-    Overwrite = "02"
-    NoOverwriteReply = "03"
-    OverwriteImpossible = "04"
-    MemoryFull = "05"
-
-
-class TerminatePacketType(Enum):
-    Default = "00"
-    UserRequest = "01"
-    Timeout = "02"
-    Overwrite = "03"
+    OptimizeFileSystem = b"51"  # Value : 81
