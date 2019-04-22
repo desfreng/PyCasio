0dde5f38a022a71a02cccb4f3f763dbd2e1d922f
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2019-04-22 22:12:38 +0200

    Reorganisation

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from PacketManager import PacketManager
+from enum import Enum
 
 
-class Calculatrice:
-    def __init__(self):
+class CalculatorMode(Enum):
+    MCSStorage = Enum.auto()
+    FlashStorage = Enum.auto()
+    SDCard = Enum.auto()
+
+
+class Calculator:
+    def __init__(self, cal_mode : CalculatorMode = CalculatorMode.MCSStorage):
+        if not isinstance(cal_mode, CalculatorMode):
+            raise TypeError
+
         self._m = PacketManager()
         self._history = []
+        self.mode = cal_mode
+
+    def list_file(self):
+        if self.mode == CalculatorMode.MCSStorage:
+            pass
